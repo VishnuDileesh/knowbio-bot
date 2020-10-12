c202e41c1336796fd4e03a51865783b07a5ce33f
Vishnu Dileesh <dev@vishnudileesh.com>
2020-10-12 15:50:20 +0530

    Add - bot help command

--- index.js
+++ index.js
@@ -9,7 +9,21 @@ client.on('ready', () => {
 })
 
 
+const prefix = '!'
 
+client.on('message', (msg) => {
 
+  if(msg.author.bot) return
+  if(!msg.content.startsWith(prefix)) return
+
+  const commandBody = msg.content.slice(prefix.length)
+
+  const command = commandBody.split(' ')[1]
+
+  if(command === undefined || command === 'help'){
+    msg.reply(':robot: Am knowbio bot :sunglasses: \n I help everyone to get to know each other easily and share their bio information :handshake: \n Commands: \n !knowbio add (for adding your bio information) \n !knowbio @username (for getting profile bio information)')
+  } 
+
+})
 
 client.login(process.env.BOT_TOKEN)
